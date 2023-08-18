e12eb22dff020479358be5edd1b82679e4a1e6cf
Oscar5323 <jr.mercado2001@gmail.com>
2023-08-17 22:12:16 -0500

    model done

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,38 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    catagory_id:{ 
+      type: DataTypes.INTEGER,
+      reference:{
+        model:"category"
+      }
+    },
+    product_name:{
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+
+    price:{
+      type: DataTypes.DECIMAL,
+      allowNull:false,
+      validate:{
+        isDecimal:true
+      }
+    },
+
+    stock:{
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      validate:{
+        isNumeric:true
+      }
+    },
   },
   {
     sequelize,
